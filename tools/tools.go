f196bdb8d4179fec187afe94f1b2b3f036b305dc
Mauri <mpino1701@gmail.com>
2023-06-02 19:43:26 -0400

    add method tools manager json

---
+++ tools/tools.go
@@ -0,0 +1,31 @@
+package tools
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+func OpenJson(path string) map[string]interface{} {
+	archive, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var f interface{}
+	json.Unmarshal(archive, &f)
+	return f.(map[string]interface{})
+
+}
+
+func ConvertString(list_interface map[string]interface{}, key_value string) map[string]string {
+	result := map[string]string{}
+	interf := list_interface[key_value].(map[string]interface{})
+
+	for k := range interf {
+		if v, ok := interf[k].(string); ok {
+			result[k] = v
+		}
+	}
+	return result
+
+}
