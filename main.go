820caaf07f8afafd892e709451a95ca3f136bc7c
Mauri <mpino1701@gmail.com>
2023-06-02 20:42:38 -0400

    add logo application

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import (
-	"github.com/Mau005/KrayManagerUpdate/schema"
-)
+import "fmt"
 
 func main() {
-	querys := schema.NewQuerys("data/config.json")
-	querys.ChangePosition(3552, 3650, 7)
-
+	fmt.Println("_  _ ____ ____ _   _ _  _ ____ _  _ ____ ____ ____ ____ ")
+	fmt.Println("|_/  |__/ |__|  \\_/  |\\/| |__| |\\ | |__| | __ |___ |__/ ")
+	fmt.Println("| \\_ |  \\ |  |   |   |  | |  | | \\| |  | |__] |___ |  \\ ")
+	fmt.Println("_  _ ____ ____ _   _ _  _ ____ _  _ ____ ____ ____ ____ ")
+	fmt.Println("Created by Mau: https://github.com/mau005")
 }
