483f03b9f2677ed1d22116d7932e2ec03d973cf6
mauricio <mpino1701@gmail.com>
2023-06-13 01:47:54 -0400

    add panel admin

--- routes/index.routes.go
+++ routes/index.routes.go
@@ -14,3 +14,13 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	template.Execute(w, nil)
 }
+
+func AdminHandler(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("templates/admin.html")
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	template.Execute(w, nil)
+}
