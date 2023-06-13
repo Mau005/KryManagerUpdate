7d8737e67ed1c08bbf13489b7fd8354def56adc5
mauricio <mpino1701@gmail.com>
2023-06-13 01:48:33 -0400

    edit structf for accounts in example json

--- models/Accounts.go
+++ models/Accounts.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type Accounts struct {
 	gorm.Model
-	Name           string `gorm:"not null" json:"name"`
-	Password       string `gorm:"not null" json:"-"`
-	Email          string `gorm:"not null;unique_index" json:"email"`
-	Type           string `gorm:"not null" json:"type"`
-	Premium_Points string `gorm:"not null;unique_index" json:"premium_points"`
+	Name           string `gorm:"not null" json:"name,omitempty"`
+	Password       string `gorm:"not null" json:"password,omitempty"`
+	Email          string `gorm:"not null;unique_index" json:"email,omitempty"`
+	Type           string `gorm:"not null" json:"type,omitempty"`
+	Premium_Points string `gorm:"not null;unique_index" json:"premium_points,omitempty"`
 }
